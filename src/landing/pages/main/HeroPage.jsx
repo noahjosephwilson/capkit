7258e52e86ba6c1652897844b2b90c12bebc357f
Jasper2023 <jasperdtripp@gmail.com>
2025-03-20 13:55:45 -0400

    Adjustments, Capability Icons

--- src/landing/pages/main/HeroPage.jsx
+++ src/landing/pages/main/HeroPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './HeroPage.module.css';
+import {
+  CapTableIcon,
+  DocumentsIcon,
+  VisualsIcon,
+  ValuationIcon,
+  FundingIcon
+} from '@/landing/Icons/Icons.jsx';
 
 function Hero() {
   return (
     <section className={styles.heroSection}>
       <div className={styles.contentWrapper}>
-        
         {/* Headline */}
         <h1 className={styles.headline}>
-            Equity management <br /> in <span className={styles.highlight}>Two Clicks</span>.
+            Your <span className={styles.highlight}> day-one </span> CFO 
         </h1>
-
         {/* Subheading */}
         <p className={styles.subheading}>
+          Manage your startup's equity in two clicks.
         </p>
         {/* Buttons */}
         <div >
@@ -20,6 +26,28 @@ function Hero() {
             Start for Free
             <span className={styles.arrow}>&rarr;</span>
           </button> 
+        </div>
+        <div className={styles.capabilitiesContainer}>
+          <button className={styles.capabilityButton}>
+            <CapTableIcon className={styles.icon} />
+            Cap Table
+          </button>
+          <button className={styles.capabilityButton}>
+            <DocumentsIcon className={styles.icon} />
+            Documents
+          </button>
+          <button className={styles.capabilityButton}>
+            <VisualsIcon className={styles.icon} />
+            Visuals
+          </button>
+          <button className={styles.capabilityButton}>
+            <ValuationIcon className={styles.icon} />
+            409A Valuation
+          </button>
+          <button className={styles.capabilityButton}>
+            <FundingIcon className={styles.icon} />
+            Funding Rounds
+          </button>
         </div>
       </div>
     </section>
